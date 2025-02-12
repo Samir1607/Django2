202fcb9284830e05fa8901c8d9af372d3119a86d
Samir1607 <samirpadekar95@gmail.com>
2025-02-12 15:03:45 +0530

    First commit

--- Employees/settings.py
+++ Employees/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xguab3sytx07!9)5=2@l76(x_61&#(wp+am0a+*(v4m9)%li@h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
